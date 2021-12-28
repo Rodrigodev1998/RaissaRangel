5adcc7e294d073a645eed9245ae2e4842a8ff74e
Rodrigodev1998 <rodrigofersouza5@gmail.com>
2021-12-28 12:11:41 -0300

    finalizando estilizacao

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Fotografa", "Social Media", "Marketing",],
+        strings: ["Fotografia", "Social Media", "Marketing",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
