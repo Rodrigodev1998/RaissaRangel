11a7a80a462fe9ba22fe128328a36a462c78a0bd
Rodrigodev1998 <rodrigofersouza5@gmail.com>
2021-12-28 06:24:17 -0300

    criado estilizacao

--- script.js
+++ script.js
@@ -13,7 +13,41 @@ $(document).ready(function(){
         $('.menu-btn i').toggleClass("active");
     });
 
-    
+    // typing text animation script
+    var typed = new Typed(".typing", {
+        strings: ["Fotografa", "Social Media", "Marketing",],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+    var typed = new Typed(".typing-2", {
+        strings: ["Fotografa", "Social Media", "Marketing",],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+        // owl carousel script
+    $('.carousel').owlCarousel({
+        margin: 20,
+        loop: true,
+        autoplay: true,
+        autoplayTimeOut: 2000,
+        autoplayHoverPause: true,
+        responsive: {
+        0:{
+        items: 1,
+        nav: false
+        },
+        600:{
+        items: 2,
+        nav: false
+        },
+        1000:{
+        items: 3,
+        nav: false
+        }
+        }
+        });
 
     
 });
